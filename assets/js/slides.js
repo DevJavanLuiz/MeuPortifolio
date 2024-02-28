dd9f99cf4daa3f3ed7132d35809d4430b5628234
Luizinho <javanlsj@gmail.com>
2024-02-28 16:32:42 -0300

    responsivo celular

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,8 +1,16 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
     },
-  });
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
